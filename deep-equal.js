575c7465b5376cf3e3e6c91ff5c536a9e8a7da27
Asian Himothy <evan.yang23@student.cart.org>
2024-01-29 10:43:46 -0800

    .

--- deep-equal.js
+++ deep-equal.js
@@ -1,17 +1,60 @@
 function deepEqual(value, reference) {
-  // Add code.
+  if (value == null || reference == null) return null;
+
+  if (value === reference) return true;
+
+  if (typeof value === 'object') {
+    for (const p in value) {
+
+      let found = false;
+
+      for (const p2 in reference) {
+
+        if (p === p2) {
+          found = true
+          console.log('1: ' + p)
+          console.log('2: ' + p2)
+        }
+
+        
+
+        if (!found) {
+          
+          if ( (typeof p === 'object') && (typeof p2 === 'object') ) {
+            found = deepEqual(p, p2)
+          } 
+          
+        }
+
+        //console.log(found)
+        //console.log(p)
+        //console.log(p2)
+
+        if (!found) break;
+
+      }
+
+      console.log(p, found)
+
+    }
+  }
+
+  return true
 }
 
-
-
-
-
-
 // tests
-let obj = {here: {is: "an"}, object: 2};
-console.log(deepEqual(obj, obj));
+let obj = {
+  here: {
+    is: "an"
+  }, 
+  object: 2
+};
+
+// console.log(deepEqual(obj, obj));
 // → true
+
 console.log(deepEqual(obj, {here: 1, object: 2}));
 // → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+
+//console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // → true
