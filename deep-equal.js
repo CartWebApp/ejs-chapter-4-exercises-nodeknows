6280746f0eccb51baceb5a619ba43124510a68b1
Asian Himothy <evan.yang23@student.cart.org>
2024-01-30 08:40:10 -0800

    .

--- deep-equal.js
+++ deep-equal.js
@@ -1,45 +1,18 @@
-function deepEqual(value, reference) {
-  if (value == null || reference == null) return null;
-
-  if (value === reference) return true;
-
-  if (typeof value === 'object') {
-    for (const p in value) {
-
-      let found = false;
-
-      for (const p2 in reference) {
-
-        if (p === p2) {
-          found = true
-          console.log('1: ' + p)
-          console.log('2: ' + p2)
+function deepEqual(x, y) {
+  if (x === y) { 
+    return true;
+  } else if ( (typeof x === 'object' && x != null) && (typeof y === 'object' && y != null) ) {
+    for (let px in x) {
+        if (y.hasOwnProperty(px)) {
+          if (!deepEqual(x[px], y[px])) return false;
+        } else {
+          return false;
         }
-
-        
-
-        if (!found) {
-          
-          if ( (typeof p === 'object') && (typeof p2 === 'object') ) {
-            found = deepEqual(p, p2)
-          } 
-          
-        }
-
-        //console.log(found)
-        //console.log(p)
-        //console.log(p2)
-
-        if (!found) break;
-
-      }
-
-      console.log(p, found)
-
     }
+    return true;
+  } else {
+    return false
   }
-
-  return true
 }
 
 // tests
@@ -50,11 +23,11 @@ let obj = {
   object: 2
 };
 
-// console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, obj));
 // → true
 
 console.log(deepEqual(obj, {here: 1, object: 2}));
 // → false
 
-//console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // → true
--- list.js
+++ list.js
@@ -1,17 +1,66 @@
+/*
+
+function arrayToList 
+- builds up a list structure like the one shown when given [1, 2, 3] as argument.
+
+function listToArray 
+- produces an array from a list.
+
+function prepend
+- takes an element & a list
+- creates a new list 
+- adds the element to the front of the input list
+
+function nth
+- takes a list and a number
+- returns the element at the given position in the list (with zero referring to the first element)
+- undefined when there is no such element.
+
+*/
+
+
+function prepend(list, elem) {
+  for (let i = 0; i != list.length; i++) {
+    console.log(i)
+  }
+}
+
+function nth() {
+
+}
+
 function arrayToList(arr) {
-  // Add code.
+  if (arr.length > 1) {
+      
+    var list = {}
+
+    for (let i = 0; i != arr.length; i++) {
+      cVal = arr[i];
+      cRest = {value: arr[i]};
+      cVal = cRest.value;
+      cRest = cRest.rest
+    }
+
+  }
+
+  console.log(list)
 }
 
 function listToArray(list, arr) {
-  // Add code.
+
 }
 
-// tests
+
+
+
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(listToArray(arrayToList([10, 20, 30])));
+
+//console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
-console.log(prepend(10, prepend(20, null)));
+
+//console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(nth(arrayToList([10, 20, 30]), 1));
+
+//console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
